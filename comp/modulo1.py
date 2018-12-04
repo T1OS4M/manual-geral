0213ee498a5829dd0ea4983f75fe86d00028b0b2
Tio Sam <45148490+T1OS4M@users.noreply.github.com>
2018-12-04 20:16:17 -0200

    Update and rename comp/modulo1.py to  comp/modulo1.py

--- comp/modulo1.py
+++ comp/modulo1.py
@@ -2,54 +2,37 @@
 import os
 import sys
 import time
-def menu():
-    print('[01]METASPLOIT')
-    time.sleep(0.5)
-    print('[02]SQLMAP')
-    time.sleep(0.5)
-    print('[03]XERXES')
-    time.sleep(0.5)
-    print('[04]COMANDOS DO TERMUX')
-    time.sleep(0.5)
-    print('[00]SAIR')
-    time.sleep(0.5)
-    n = int(input('\n mnl: \n'))
-    if n == 1 or n == '01':
-        metasploit()
-    elif n == 2 or n == '02':
-        sql()
-    elif n == 3 or n == '03':
-        xerxes()
-    elif n == 4 or n == '04':
-        termux()
-    elif n == 00:
-        sys.exit()
-    else:
-        print('\033[31m OPÇÃO INVALIDA \033[m \n')
-        menu()
+def backtomenu():
+    os.system('python3 manual.py')
 def mts():
-    print('...Instalando Metasploit...')
+    print('\n...Instalando Metasploit...\n')
+    time.sleep(1)
     os.system('apt update && apt upgrade')
     os.system('apt install git')
     os.system('git clone https://github.com/rapid7/metasploit-framework')
     os.system('mv metasploit-framework ~ ')
-    print('Terminado!!!')
-    menu()
+    print('\n Terminado!!! \n')
+    time.sleep(0.5)
+    backtomenu()
 def sqlmap():
-    print('...Instalando sqlmap...')
+    print('\n...Instalando sqlmap...\n')
+    time.sleep(1)
     os.system('apt update && apt upgrade')
     os.system('apt install git')
     os.system('git clone https://github.com/sqlmapproject/sqlmap')
     os.system('mv sqlmap ~ ')
-    print('Terminado!!!')
-    menu()
+    time.sleep(0.5)
+    print('\n Terminado!!! \n')
+    backtomenu()   
 def xerxesin():
-    print('...Instalando xerxes...')
+    print('\n...Instalando xerxes...\n')
+    time.sleep(1)
     os.system('apt upadte && apt upgrade')
     os.system('apt install git')
     os.system('apt install clang')
     os.system('git clone https://github.com/zanyarjamal/xerxes')
     os.system('cd ~ / xerxes && clang xerxes.c -o xerxes')
-    print('Terminado!!!')
-    menu()
+    print('\n Terminado!!! \n')
+    time.sleep(0.5)
+    backtomenu()
 
